e40b8f284f6bd2ca41d166a8374f37b5b70cd255
Bruhhh211 <akmalhfd087@gmail.com>
2024-07-20 14:39:42 +0700

    tomarket.py

--- tomarket.py
+++ tomarket.py
@@ -300,7 +300,7 @@ def print_welcome_message():
 █▄█ █▀█ █▀█ █▄▄ █ █▄█ █ ██▄
           """)
     print(Fore.GREEN + Style.BRIGHT + "Tomarket BOT")
-    print(Fore.CYAN + Style.BRIGHT + "Update Link: https://github.com/adearman/tomarket>
+    print(Fore.CYAN + Style.BRIGHT + "Update Link: https://github.com/Bruhhh211/TmT>
     print(Fore.YELLOW + Style.BRIGHT + "Free Konsultasi Join Telegram Channel: https://>
     print(Fore.BLUE + Style.BRIGHT + "Buy me a coffee :) 0823 2367 3487 GOPAY / DANA")
     print(Fore.RED + Style.BRIGHT + "NOT FOR SALE ! Ngotak dikit bang. Ngoding susah2 k>
